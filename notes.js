95b132f23f6b19bd804216fc00d3f33195f9b108
EdibleTwix <dvr.sf45@gmail.com>
2024-09-17 18:37:54 -0400

    notes page

---
+++ notes.js
@@ -0,0 +1,22 @@
+// all html files 
+    // misaligned <head></head>
+    // reduce syntax 'align="center"'
+
+// missing html files
+    // database.html
+    // current-projects.html (i assume this is research.htmnl)
+
+// support-the-herbarium
+    // has un-needed table
+    // i believe it was renamed before to just 'support.html'
+
+// search-our-collections
+    // href to third party website without displaying information on the html file
+
+// index
+    // transfer to a new file called 'home.html', with redirect from index.html
+
+// resources
+    // redo table for @media, size constraints and legibility
+
+
