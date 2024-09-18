8de86f89541c00ac9aa06b5b86c878d772d18e23
EdibleTwix <dvr.sf45@gmail.com>
2024-09-17 22:25:57 -0400

    notes

--- notes.js
+++ notes.js
@@ -8,15 +8,29 @@
 
 // support-the-herbarium
     // has un-needed table
-    // i believe it was renamed before to just 'support.html'
+    // i believe it was renamed before to just 'support.html' to match dr. brown's email signature link
 
 // search-our-collections
-    // href to third party website without displaying information on the html file
+    // href to third party website without displaying information on the html file itself
 
 // index
-    // transfer to a new file called 'home.html', with redirect from index.html
+    // transfer to a new file called 'home.html', with redirect from index.html. provided index.html as example.
+/*  < !DOCTYPE html >
+    <html lang="en">
+        <head>
+            <meta http-equiv="refresh" content="0; url=home.html">
+                <title>Redirecting...</title>
+        </head>
+        <body>
+            <p>Follow this <a href="home.html">link</a> to be redirected.</p>
+        </body>
+    </html> */
+
 
 // resources
     // redo table for @media, size constraints and legibility
+
+// course-offerings
+    // rename to 'courses'?
 
 
