df08d724c0b1137531f38ed258a63c81e6a6fd46
EdibleTwix <dvr.sf45@gmail.com>
2024-09-23 20:08:50 -0400

    footer.html and loadFooter.js, social links

---
+++ scripts/loadFooter.js
@@ -0,0 +1,10 @@
+function loadFooter() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
+}
+
+document.addEventListener('DOMContentLoaded', loadFooter);
